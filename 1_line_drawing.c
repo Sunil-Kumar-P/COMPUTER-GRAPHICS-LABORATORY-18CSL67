24090825a7923d406551b99ea8cec609900bab31
Sunil Kumar P <sunilkpg23@gmail.com>
2023-07-15 11:19:37 +0530

    Update 1_line_drawing.c

--- 1_line_drawing.c
+++ 1_line_drawing.c
@@ -1,7 +1,8 @@
 #include<GL/glut.h>
 #include<stdio.h>
 
-float x0,y0,x1,y1,df,dx,dy;
+float x0,y0,x1,y1,df,dx,dy;	
+int i;
 void init()
 {
 	gluOrtho2D(0,500,0,500);
@@ -10,7 +11,6 @@ void display()
 {
 	glClearColor(1,1,1,1);
 	glClear(GL_COLOR_BUFFER_BIT);
-	int i;
 	dy=abs (y1-y0);
 	dx=abs (x1-x0);
 	df=(2*dy)-dx;
